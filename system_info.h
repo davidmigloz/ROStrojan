f0623526c5abf674e34da99c0c8b2f9c8a9c63fa
Jazriel <javyermartinez@gmail.com>
2016-09-20 19:58:46 +0200

    #10 Completed ver_equipo function.

--- system_info.h
+++ system_info.h
@@ -6,18 +6,33 @@
 #define ROSTROJAN_SYSTEM_INFO_H
 #endif //ROSTROJAN_SYSTEM_INFO_H
 
-#include <unistd.h>
+#define H_N_LENGTH 20
+
+#include <unistd.h>  //To include some functionalities specified in POSIX
+#include <stdlib.h>
 
 
 char* ver_usuarioActual();
-
-
+int ver_equipo(char* hostname, int length);
 
 /**
- * ver_usuarioActual; function that returns a pointer to a string containing the user name of the caller.
+ * ver_usuarioActual: function that returns a pointer to a string containing the user name of the caller.
  * Do not use this function in segure or critical programs as is easily deceivable.
  * @return pointer to a string
  */
 char* ver_usuarioActual() {
     return getlogin();
+}
+
+/**
+ * ver_equipo: function that returns a pointer to a string containing the name of the host.
+ * @return pointer to a string or null in operation failure
+ */
+int ver_equipo(char* hostname, int length){
+    int errcheck = gethostname(hostname, length);
+    if(errcheck==0){
+        return 0;
+    }else{
+        exit(1);
+    }
 }
