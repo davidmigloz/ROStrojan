033ba408a3499231e2e07ed7230e4408573be5ab
Jazriel <javyermartinez@gmail.com>
2016-09-20 16:34:06 +0200

    #9 Completed function ver_usuarioActual().

---
+++ file_mng.h
@@ -0,0 +1,8 @@
+//
+// Created by jack on 20/09/16.
+//
+
+#ifndef ROSTROJAN_FILE_MNG_H
+#define ROSTROJAN_FILE_MNG_H
+
+#endif //ROSTROJAN_FILE_MNG_H
---
+++ system_info.h
@@ -0,0 +1,8 @@
+//
+// Created by jack on 20/09/16.
+//
+
+#ifndef ROSTROJAN_SYSTEM_INFO_H
+#define ROSTROJAN_SYSTEM_INFO_H
+
+#endif //ROSTROJAN_SYSTEM_INFO_H
