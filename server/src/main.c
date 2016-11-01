eacc3a8070661b1c25c2f6516f138a4fc1ecec8e
Jazriel <javyermartinez@gmail.com>
2016-11-01 18:27:35 +0100

    Started to implement #22 #23 #29.

--- server/src/main.c
+++ server/src/main.c
@@ -1,5 +1,90 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+#include <sys/sem.h>
+#include <unistd.h>
+#include "shm_mng.h"
 
 int main() {
+    device devices[N_OF_DEVICES];
+    char* shm_address;
+
+    shm_address = init_shm();
+
+    set_n_of_devices(shm_address, 10);
+
+    int i = get_n_of_devices(shm_address);
+
+    printf("%d", i);
+
+    tear_shm(shm_address);
+    /*
+    //Init semaphore
+    key_t sem_key;
+    int sem_id;
+    struct sembuf sem;
+
+    if ((sem_key = ftok( "/tmp/rostrojan", 's'))==(key_t)-1){
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
+    if ((sem_id = semget(sem_key, 1,IPC_CREAT | IPC_EXCL ))==-1){
+        perror("semget error\n");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        sem.sem_num = 0;
+        sem.sem_op = 1;
+        sem.sem_flg = 0;
+        if (semop(sem_id, &sem, 1) == -1) {
+            perror("semop error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    //End semaphore init
+    //Start fork
+    int pid;
+    int son_exit;
+    int parent_exit;
+
+    switch(pid = fork()) {
+        case -1:
+            perror("fork error\n");
+            break;
+        case 0:
+            son_exit = son_proccess();
+            exit(son_exit);
+        default:
+            parent_exit = parent_proccess();
+            break;
+    }
+    //End fork
+    */
     return 0;
 }
+
+
+int son_proccess(){
+    /*Son does stuff*/
+
+    /*Añadir cosas falsas*/
+    return EXIT_SUCCESS;
+    /*Son stops doing stuff*/
+}
+
+
+
+int parent_proccess(){
+    /*Parent does stuff*/
+    while(1){
+        /*Menu hasta que haya que cerrar*/
+
+
+        return EXIT_SUCCESS;
+    }
+    return EXIT_SUCCESS;
+    /*Parent stops doing stuff*/
+}
+
---
+++ server/src/shm_mng.c
@@ -0,0 +1,81 @@
+#include "shm_mng.h"
+
+/**
+ * Crea una estructura IPC y la añade a la memomria
+ * @return pointer to shm
+ */
+char* init_shm(){
+    int shm_size = sizeof(device)*N_OF_DEVICES+sizeof(int);
+
+    key_t mem_key;
+    int shm_id;
+    char* shm_address;
+    /*
+     * hay que crear el '/tmp/rostrojan' si queremos hacerlo sobre /tmp/rostrojan
+     */
+    //Conseguimos una estructura IPC quasi unica con ftok
+    if ((mem_key = ftok( "/tmp", 'm')) == (key_t)-1){
+        perror("SHM: ftok error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Creamos la memoria compartida en esa estructura IPC con permisos de lectura escritura para el usuario unicamente
+    if ((shm_id = shmget(mem_key, shm_size, IPC_CREAT | IPC_EXCL | 0600)) < 0) {
+        perror("SHM: shmget error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Añadimos esa memoria compartida a nuestro proceso
+    if ((shm_address = shmat(shm_id, NULL, 0)) == (char *) -1) {
+        perror("SHM: shmat error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Nos aseguramos que el sistema sea capaz de limpiar esa estructura IPC cuando no haya procesos usandola
+    if (shmctl(shm_id, IPC_RMID, NULL)==-1){
+        perror("SHM: shmctl error\n");
+        exit(EXIT_FAILURE);
+    }
+    return shm_address;
+}
+
+/**
+ * Quitamos la memoria compartida de nuestra aplicacion
+ * @param shm_pos pointer to the shared memory
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
+int tear_shm(char* shm_pos){
+    //Des-añadimos la memoria compartida de nuestro proceso
+    if(shmdt(shm_pos)==-1){
+        perror("SHM: shmdt error\n");
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
+
+/**
+ * Getter para el n_of_devices para que los usuarios de la biblioteca no tengan que conocer la implementacion.
+ * @param shm_pos
+ * @return int del n_of_devices
+ */
+int get_n_of_devices(char* shm_pos){
+    return (int) *shm_pos;
+}
+
+/**
+ * Setter para el n_of_devices para que los usuarios de la biblioteca no tengan que conocer la implementacion.
+ * @param shm_pos
+ * @param n_of_devices
+ */
+void set_n_of_devices(char* shm_pos, int n_of_devices){
+    (*(int *)(shm_pos)) = n_of_devices;
+}
+
+device get_n_device(char* shm_pos, int n){
+    return (device)*(device*)(shm_pos + sizeof(int) + sizeof(device)*n);
+}
+
+device* get_n_device_address(char* shm_pos, int n){
+    return (device*)(shm_pos + sizeof(int) + sizeof(device)*n);
+}
+
