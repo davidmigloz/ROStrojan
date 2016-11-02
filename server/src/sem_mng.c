0339e4d5ec718ab22aef3d94f6d4d55510cda4f5
Jazriel <javyermartinez@gmail.com>
2016-11-02 16:25:51 +0100

    Added documentation for #24

--- server/src/sem_mng.c
+++ server/src/sem_mng.c
@@ -8,20 +8,27 @@
 
 #include "sem_mng.h"
 
+/**
+ * Inicializa el semaforo
+ * @return id de semaforo
+ */
 int init_sem() {
     key_t sem_key;
     int sem_id;
     struct sembuf sem;
 
+    // Pido key de estructura IPC a ftok
     if ((sem_key = ftok( "/tmp", 's'))==(key_t)-1){
         perror("SEM: ftok error\n");
         exit(EXIT_FAILURE);
     }
 
-    if ((sem_id = semget(sem_key, 1,IPC_CREAT | IPC_EXCL ))==-1){
+    // Pido un conjunto de semaforos nuevo con un semaforo
+    if ((sem_id = semget(sem_key, 1,IPC_CREAT | IPC_EXCL | 0600))==-1){
         perror("SEM: semget error\n");
         exit(EXIT_FAILURE);
     }else{
+        // Inicializo el semaforo a ser el 0 del array con 1 recurso sin NOWAIT para que tengan que esperar
         sem.sem_num = 0;
         sem.sem_op = 1;
         sem.sem_flg = 0;
@@ -34,14 +41,27 @@ int init_sem() {
     return sem_id;
 }
 
+/**
+ * Elimina el semaforo
+ * @param sem_id id del semaforo
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
 int tear_sem(int sem_id){
+    // Elimino el semaforo
     if (semctl(sem_id, IPC_RMID, NULL) == -1) {
         perror("SEM: semctl error\n");
         exit(EXIT_FAILURE);
     }
+    return EXIT_SUCCESS;
 }
 
+/**
+ * Signal o V al semaforo
+ * @param sem_id id del semaforo
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
 int signal_sem(int sem_id){
+    // Incremento el semaforo en uno
     struct sembuf sem_op = { 0, 1, NULL };
     if((semop(sem_id, &sem_op, 1) == -1){
         perror("SEM: semop signal error\n");
@@ -50,7 +70,13 @@ int signal_sem(int sem_id){
     return EXIT_SUCCESS;
 }
 
+/**
+ * Wait o P al semaforo
+ * @param sem_id id del semaforo
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
 int wait_sem(int sem_id){
+    // Decremento el semaforo en uno
     struct sembuf sem_op = { 0, -1, NULL };
     if((semop(sem_id, &sem_op, 1) == -1){
         perror("SEM: semop wait error\n");
--- server/src/sem_mng.h
+++ server/src/sem_mng.h
@@ -14,4 +14,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//PROTOTYPES
+/**
+ * Inicializa el semaforo
+ * @return id de semaforo
+ */
+int init_sem();
+/**
+ * Elimina el semaforo
+ * @param sem_id id del semaforo
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
+int tear_sem(int sem_id);
+/**
+ * Signal o V al semaforo
+ * @param sem_id id del semaforo
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
+int signal_sem(int sem_id);
+/**
+ * Wait o P al semaforo
+ * @param sem_id id del semaforo
+ * @return EXIT_SUCCESS o EXIT_FAILURE
+ */
+int wait_sem(int sem_id);
+
 #endif //SERVER_SEM_MNG_H
